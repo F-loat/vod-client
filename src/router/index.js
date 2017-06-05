ce6f8e0bdcb058bff5e887459dcc9d3be8a01b78
F-loat <945852046@qq.com>
2017-06-05 22:38:24 +0800

    fix vue-router code-splitting

--- src/router/index.js
+++ src/router/index.js
@@ -1,48 +1,48 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 
-const index = import('@/views/video');
-const list = import('@/views/video/list');
-const play = import('@/views/video/play');
-const forum = import('@/views/forum');
-const topic = import('@/views/forum/detail');
-const login = import('@/views/login');
-const notFound = import('@/views/404');
+const index = () => import('@/views/video');
+const list = () => import('@/views/video/list');
+const play = () => import('@/views/video/play');
+const forum = () => import('@/views/forum');
+const topic = () => import('@/views/forum/detail');
+const login = () => import('@/views/login');
+const notFound = () => import('@/views/404');
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
-    component: () => index,
+    component: index,
   },
   {
     path: '/detection',
-    component: () => list,
+    component: list,
   },
   {
     path: '/forum',
-    component: () => forum,
+    component: forum,
   },
   {
     path: '/forum/:id',
-    component: () => topic,
+    component: topic,
   },
   {
     path: '/list/:type',
-    component: () => list,
+    component: list,
   },
   {
     path: '/play/:id/:episode',
-    component: () => play,
+    component: play,
   },
   {
     path: '/login',
-    component: () => login,
+    component: login,
   },
   {
     path: '*',
-    component: () => notFound,
+    component: notFound,
   },
 ];
 
